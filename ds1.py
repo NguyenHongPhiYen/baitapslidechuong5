ea0e57a9b79765c25cdb5740190f488585e6c4f5
NguyenHongPhiYen <nguyenhongphiyen29@gmail.com>
2024-09-15 12:09:17 +0700

    Add files via upload

---
+++ ds1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 23:44:19 2024
+
+@author: Admin
+"""
+
+import random
+slpt= random.randint(20, 30)
+so_random= random.randint(1, 100)
+for so_random in slpt:
+  print(f"Số lượng phần tử:{slpt}")
+print("Danh sách các số ngẫu nhiên:")
+print(so_random)
